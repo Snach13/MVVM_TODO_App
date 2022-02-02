e509d13a6195685903bf10e392100caa9ae6c014
Snach13 <nachiketgomkale15@gmail.com>
2022-02-02 23:35:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvmtodoapp/data/TodoRepoImpl.kt
@@ -0,0 +1,24 @@
+package com.example.mvvmtodoapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class TodoRepoImpl(
+    private val dao: TodoDao
+) : TodoRepository {
+
+    override suspend fun insertTodo(todo: Todo) {
+        dao.insertTodo(todo)
+    }
+
+    override suspend fun deleteTodo(todo: Todo) {
+        dao.deleteTodo(todo)
+    }
+
+    override suspend fun getTodoById(id: Int): Todo? {
+        return dao.getTodoById(id)
+    }
+
+    override fun getTodos(): Flow<List<Todo>> {
+         return dao.getTodos()
+    }
+}
